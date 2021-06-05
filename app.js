7b4e6e4d0bd4c516c61be0759eef4c07d9db6540
matteo-andreuzza <76852004+matteo-andreuzza@users.noreply.github.com>
2021-06-05 23:04:03 +0200

    animazione vecchia area

---
+++ app.js
@@ -0,0 +1,8 @@
+function mostra() {
+    document.getElementById('ex').style.display = "block";
+}
+window.setTimeout("mostra()", 10000);
+
+function ric() {
+    document.getElementById('ex').style.display = "none";
+}
